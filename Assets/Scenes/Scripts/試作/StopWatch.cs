f471edf27e3e0796902ed080757e519f2553fb5b
2321025-PC\2321025 <donbei796@gmail.com>
2024-12-11 15:53:58 +0900

    新シーン追加、ライフ制着手

--- Assets/Scenes/Scripts/試作/StopWatch.cs
+++ Assets/Scenes/Scripts/試作/StopWatch.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //タイム計測用クラス
+//一つの独立したもの、あるスクリプトで計測開始し、もう一方で計測終了させることができる。
 public static class StopWatch
 {
     private static System.Diagnostics.Stopwatch stopwatch
@@ -27,5 +28,33 @@ public static class StopWatch
     private static bool isStart()
     {
         return 0 == i++;
+    }
+}
+namespace local
+{
+    public class StopWatch
+    {
+        private static System.Diagnostics.Stopwatch stopwatch
+        = new();
+        private static int i = 0;
+        public static void Start()
+        {
+            if (isStart())
+            {
+                stopwatch.Restart();
+            }
+        }
+
+        public static TimeSpan StopAndGetTime()
+        {
+            stopwatch.Stop();
+            i = 0;
+            return stopwatch.Elapsed;
+        }
+
+        private static bool isStart()
+        {
+            return 0 == i++;
+        }
     }
 }
