4f965b53dc2e66f788a79da989f99f6d13552322
SorAct <akuto0812@gmail.com>
2024-12-12 20:55:18 +0900

    回数制限完成 55行目の数字で回数を決められます

--- Assets/Scenes/Scripts/試作/phoneManagerLife.cs
+++ Assets/Scenes/Scripts/試作/phoneManagerLife.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -22,6 +20,7 @@ public class phoneManagerLife : MonoBehaviour
         Container target = new(phone);
         target &= (-4, 0, 10);
     }
+
     private void BackInitialRotate(GameObject phone)
     {
         Transform phoneTrs = phone.GetComponent<Transform>();
@@ -31,16 +30,19 @@ public class phoneManagerLife : MonoBehaviour
     int maxcycle = 2;//最大で着信が来ない回数
     int currentcycle = 0; //電話が来た回数？
     int watingsum = 0;
-    int phonecalltime = 0;
-    bool isCallAble = true;
+
+    bool isCallAble = true;//前回のサイクルで電話が来なかったらtrue、来たらfalse
+    int failedTime = 0;
     //時間制御なんでIEnumeratorによるコルーチン
     IEnumerator waitCall()
     {
         for (; ; )
         {
+
             watingtime = Random.Range(1, 6);
-            UnityEngine.Debug.Log(watingtime);
-            if ((watingtime <= 2 || watingsum >= 8) && isCallAble)
+            if (watingtime <= 2
+            && watingsum >= 8
+            && isCallAble)
             {
                 mesh.material = colors.materials[index = 0];//ここで赤色にする
                 isCallAble = false;
@@ -48,19 +50,23 @@ public class phoneManagerLife : MonoBehaviour
                 yield return new WaitForSeconds(watingtime);
 
                 StopWatch.StopAndGetTime();
+                if (mesh.material.color == colors.materials[index = 0].color)//電話取るのに失敗した場合
+                {
+                    //TODO:失敗したとき処理
+                    failedTime++;
+                    UnityEngine.Debug.Log(failedTime);
+                    if (failedTime >= 3)
+                    {
+                        break;
+                    }
+                    continue;//ここより下に行かない
+                }
                 BackInitialPositoin(phoneObj);
                 BackInitialRotate(phoneObj);
                 currentcycle = 0; //これで無限ループ？
-                phonecalltime += 1;
-                if (phonecalltime >= 2)//2回でやってみる
-                {
-                    UnityEngine.Debug.Log("Finish!");
-                    break;
-                }
             }
             else
             {
-                //TODO:失敗したとき処理
                 isCallAble = true;
                 mesh.material = colors.materials[index = 1];
                 watingsum += watingtime;
@@ -79,6 +85,7 @@ public class phoneManagerLife : MonoBehaviour
         mesh = phoneObj.GetComponent<MeshRenderer>();
         colors = manager.GetComponent<MeshRenderer>();
         mesh.material = colors.materials[0];
-        StartCoroutine(waitCall());
+        StartCoroutine(waitCall());//下も動く
+        UnityEngine.Debug.Log("Finished");
     }
 }
