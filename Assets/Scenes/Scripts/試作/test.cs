eb0e591855f0c50a498a1d69ecadef8aee8e1d24
2321025-PC\2321025 <donbei796@gmail.com>
2024-12-17 13:50:30 +0900

    通話アイコン追加

--- Assets/Scenes/Scripts/試作/test.cs
+++ Assets/Scenes/Scripts/試作/test.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class test : MonoBehaviour
@@ -7,12 +8,11 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
