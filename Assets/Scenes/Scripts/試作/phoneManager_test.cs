aa83a93540f281ab01dfde8ab85409216fc6d720
SorAct <akuto0812@gmail.com>
2024-12-19 22:43:40 +0900

    タイムを取得した後にスコア計算して合計スコアが出るようにした

---
+++ Assets/Scenes/Scripts/試作/phoneManager_test.cs
@@ -0,0 +1,124 @@
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using GameObjectlib;
+using System.Threading;
+using System.Diagnostics;
+using local;
+using UnityEditor.Experimental.GraphView;
+using TMPro;
+using System;
+public class phoneManager_test : MonoBehaviour
+{
+    private GameObject phoneObj;
+    private GameObject manager;
+    private int watingtime;
+    private MeshRenderer mesh;
+    private int index = 0;
+    private MeshRenderer colors;
+    private TextMesh TimeText;
+    private local.StopWatch stopwath;
+    private void BackInitialPositoin(GameObject phone)
+    {
+        Container target = new(phone);
+        target &= (-4, 0, 10);
+    }
+    private void BackInitialRotate(GameObject phone)
+    {
+        Transform phoneTrs = phone.GetComponent<Transform>();
+        phoneTrs.eulerAngles = new Vector3(0, 90, 0);
+    }
+    private void Calcurate()
+    {
+        score = 25 * (4 - phonetimes[phonecalltime]);//仮の計算式なので後で変更
+        Intscore[phonecalltime] = (int)Math.Round(score);
+    }
+    private void Total()
+    {
+        foreach(var one in Intscore)
+        {
+            totalscore += one;
+        }
+    }    
+    private static int timeCountSize = 3;//変数名はわかりやすいやつで自由に、今回は3
+    int maxcycle = 2;//最大で着信が来ない回数
+    int currentcycle = 0; //電話が来た回数？
+    int watingsum = 0;
+    int phonecalltime = 0;
+    double score = 0; 
+    int totalscore = 0;
+    int[] Intscore = new int[timeCountSize];
+    private double[] phonetimes = new double[timeCountSize];
+    bool isCallAble = true;
+    //時間制御なんでIEnumeratorによるコルーチン
+    IEnumerator waitCall()
+    {
+        
+        for (; ; )
+        {
+            watingtime = UnityEngine.Random.Range(1, 6);
+            //UnityEngine.Debug.Log(watingtime);
+            if ((watingtime <= 2 || watingsum >= 8) && isCallAble)
+            {
+                mesh.material = colors.materials[index = 0];//ここで赤色にする
+                isCallAble = false;
+
+                yield return new WaitForSeconds(watingtime);
+
+                StopWatch.StopAndGetTime();
+                BackInitialPositoin(phoneObj);
+                BackInitialRotate(phoneObj);
+                var time = StopWatch.StopAndGetTime();
+                double totalTime = (time.Seconds) + (time.Milliseconds / 1000F);//ストップウォッチの値取得
+                phonetimes[phonecalltime] = totalTime;
+                Calcurate();
+                UnityEngine.Debug.Log(phonetimes[phonecalltime]);
+                UnityEngine.Debug.Log(Intscore[phonecalltime]);
+
+                
+            
+
+
+                currentcycle = 0; //これで無限ループ？
+                phonecalltime += 1;
+                if (phonecalltime == 3)//3回でやってみる
+                {
+                    UnityEngine.Debug.Log("Finish!");
+                    Total();
+                    UnityEngine.Debug.Log(totalscore);
+                    break;
+                }
+            }
+            else
+            {
+                //TODO:失敗したとき処理
+                isCallAble = true;
+                mesh.material = colors.materials[index = 1];
+                watingsum += watingtime;
+                yield return new WaitForSeconds(watingtime);
+            }
+        }
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        phoneObj = this.gameObject;
+        manager = GameObject.Find("manager");
+        mesh = GameObject.Find("display").GetComponent<MeshRenderer>();
+        colors = manager.GetComponent<MeshRenderer>();
+        mesh.material = colors.materials[0];
+        StartCoroutine(waitCall());//ちなみにここから下も動く
+       // phoneObj = this.gameObject;
+       // manager = GameObject.Find("manager");
+       // mesh = phoneObj.GetComponent<MeshRenderer>();
+       // colors = manager.GetComponent<MeshRenderer>();
+       // mesh.material = colors.materials[0];
+       // StartCoroutine(waitCall());
+
+    }
+}
